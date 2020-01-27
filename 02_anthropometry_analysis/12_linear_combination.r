072d19d224bf85d905a5412a9fdd5545328a47b4
TiysEvgeny <tiys@ngs.ru>
2020-01-27 16:00:25 +0700

    Add script to count Trait_i-SH linear combination

--- 02_anthropometry_analysis/12_linear_combination.r
+++ 02_anthropometry_analysis/12_linear_combination.r
@@ -1,12 +1,12 @@
 library(data.table)
 library(dplyr)
 
-path_to_result_directory<-'../data/anthropometry_results/four_traits/GWAS/scaled_filtered/'
+path_to_result_directory<-'../../data/anthropometry_results/four_traits/GWAS/scaled_filtered/'
 gwas_files<-list.files(path_to_result_directory, full.names=T, pattern="ID_\\d+.csv")
 gwas<-lapply(gwas_files, fread)
 
-aa<-read.table('../data/anthropometry_results/four_traits/alphas.txt', row.names=1)
-covm<-read.table('../data/anthropometry_results/four_traits/pheno_corr_matrix.txt', row.names=1,  check.names=F)
+aa<-read.table('../../data/anthropometry_results/four_traits/alphas.txt', row.names=1)
+covm<-read.table('../../data/anthropometry_results/four_traits/pheno_corr_matrix.txt', row.names=1,  check.names=F)
 
 rs_id<-lapply(gwas, function(x) x$rs_id)
 snps<-Reduce(intersect,rs_id)
@@ -59,18 +59,76 @@ GWAS_linear_combination=function(a,beta_a,se,var_y=rep(1,length(a)),covm,N){
 	return(out)
 }
 
+cov_gi_alpha=function(a,i,covm=covm){
+  cov_gi_sum_giai=sum(covm[i,]*a)
+  var_gi=covm[i,i]
+  var_sum_giai=sum(covm*(a%o%a)) #gen(a)
+  
+  cor_g_a=cov_gi_sum_giai
+  return(cor_g_a)
+} 
 
-sh_gwas=GWAS_linear_combination(a=c(1,0,0,0)-as.numeric(aa[2,])*b),beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+alphas=as.numeric(aa[2,])
+b1=cov_gi_alpha(alphas,1,as.matrix(covm))
 
-sh_gwas=mutate(sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-sh_gwas=mutate(sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-sh_gwas=mutate(sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
+tr1_sh_gwas=GWAS_linear_combination(a=c(1,0,0,0)-alphas*b1,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+
+tr1_sh_gwas=mutate(tr1_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+tr1_sh_gwas=mutate(tr1_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
+tr1_sh_gwas=mutate(tr1_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
 			 eaf=gwas_reordered[[1]]$eaf)
 
-head(sh_gwas,n=2)
-dir.create('../data/anthropometry_results/four_traits/linear_combination/')
+head(tr1_sh_gwas,n=2)
 data.table::fwrite(
-	sh_gwas, 
+	tr1_sh_gwas, 
 	row.names=F,
-	file = '../data/anthropometry_results/four_traits/linear_combination/Tr4049-SH_GWAS.txt',
+	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4049-SH_GWAS.txt',
+	sep = '\t')
+	
+b2=cov_gi_alpha(alphas,2,as.matrix(covm))
+
+tr2_sh_gwas=GWAS_linear_combination(a=c(0,1,0,0)-alphas*b2,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+
+tr2_sh_gwas=mutate(tr2_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+tr2_sh_gwas=mutate(tr2_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
+tr2_sh_gwas=mutate(tr2_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
+			 eaf=gwas_reordered[[1]]$eaf)
+
+head(tr2_sh_gwas,n=2)
+data.table::fwrite(
+	tr2_sh_gwas, 
+	row.names=F,
+	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4050-SH_GWAS.txt',
+	sep = '\t')
+
+b3=cov_gi_alpha(alphas,3,as.matrix(covm))
+
+tr3_sh_gwas=GWAS_linear_combination(a=c(0,0,1,0)-alphas*b3,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+
+tr3_sh_gwas=mutate(tr3_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+tr3_sh_gwas=mutate(tr3_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
+tr3_sh_gwas=mutate(tr3_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
+			 eaf=gwas_reordered[[1]]$eaf)
+
+head(tr3_sh_gwas,n=2)
+data.table::fwrite(
+	tr3_sh_gwas, 
+	row.names=F,
+	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4058-SH_GWAS.txt',
+	sep = '\t')
+
+b4=cov_gi_alpha(alphas,4,as.matrix(covm))
+
+tr4_sh_gwas=GWAS_linear_combination(a=c(0,0,0,1)-alphas*b4,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+
+tr4_sh_gwas=mutate(tr4_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+tr4_sh_gwas=mutate(tr4_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
+tr4_sh_gwas=mutate(tr4_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
+			 eaf=gwas_reordered[[1]]$eaf)
+
+head(tr4_sh_gwas,n=2)
+data.table::fwrite(
+	tr4_sh_gwas, 
+	row.names=F,
+	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4179-SH_GWAS.txt',
 	sep = '\t')
