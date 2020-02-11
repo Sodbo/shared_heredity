fd2dea0e4cb2c9462720a92667b0990e66116a70
TiysEvgeny <tiys@ngs.ru>
2020-02-11 19:31:32 +0700

    Correct mistake of taking phen_cov matrix instead of gene_cov andadd possibility of script to work with any number of phenotypic traits

--- 02_anthropometry_analysis/12_linear_combination.r
+++ 02_anthropometry_analysis/12_linear_combination.r
@@ -6,7 +6,8 @@ gwas_files<-list.files(path_to_result_directory, full.names=T, pattern="ID_\\d+.
 gwas<-lapply(gwas_files, fread)
 
 aa<-read.table('../../data/anthropometry_results/four_traits/alphas.txt', row.names=1)
-covm<-read.table('../../data/anthropometry_results/four_traits/pheno_corr_matrix.txt', row.names=1,  check.names=F)
+phem<-read.table('../../data/anthropometry_results/four_traits/pheno_corr_matrix.txt', row.names=1,  check.names=F)
+gcov<-read.table('../../data/anthropometry_results/four_traits/gene_cov_matrix.txt', row.names=1,  check.names=F)
 
 rs_id<-lapply(gwas, function(x) x$rs_id)
 snps<-Reduce(intersect,rs_id)
@@ -68,67 +69,60 @@ cov_gi_alpha=function(a,i,covm=covm){
   return(cor_g_a)
 } 
 
+H2=function(a,covm=covm,phem=phem){
+  h=sum(covm*(a%o%a))/sum(phem*(a%o%a))
+  return(h)
+}
+
+# genetic correlation of linear combination with a coeeficient a1 and a coeeficient a2
+cor_gi_a1_a2=function(a1,a2,covm=covm){
+  cov_gi_sum_g_a1_a2=sum(covm*(a1%o%a2))
+  var_g_a1=sum(covm*(a1%o%a1))
+  var_g_a2=sum(covm*(a2%o%a2))
+  
+  cor_g_a=cov_gi_sum_g_a1_a2/sqrt(var_g_a1*var_g_a2)
+  return(cor_g_a)
+}
+# genetic correlation of linear combination with a coeeficient with i trait
+cor_gi_alfa=function(a,i,covm=covm){
+  cov_gi_sum_giai=sum(covm[i,]*a)
+  var_gi=covm[i,i]
+  var_sum_giai=sum(covm*(a%o%a)) #gen(a)
+  
+  cor_g_a=cov_gi_sum_giai/sqrt(var_gi*var_sum_giai)
+  return(cor_g_a)
+}
+# genetic covaiance of linear combination with a coeeficient a1 and a coeeficient a2
+cov_gi_a1_a2=function(a1,a2,covm=covm){
+  cov_gi_sum_g_a1_a2=sum(covm*(a1%o%a2))
+  var_g_a1=sum(covm*(a1%o%a1))
+  var_g_a2=sum(covm*(a2%o%a2))
+  
+  cor_g_a=cov_gi_sum_g_a1_a2
+  return(cor_g_a)
+}
+
+
 alphas=as.numeric(aa[2,])
-b1=cov_gi_alpha(alphas,1,as.matrix(covm))
+n_traits=c(1:length(alphas))
+slope=sapply(n_traits,function(x) cov_gi_alpha(a=alphas,i=x,covm=as.matrix(gcov))/H2(alphas,covm=gcov,phem=phem))
+position<-diag(length(alphas))
 
-tr1_sh_gwas=GWAS_linear_combination(a=c(1,0,0,0)-alphas*b1,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+#cor_gi_a1_a2(a1=alphas,a2=position[x,]-alphas*slope[x],covm=gcov)
 
-tr1_sh_gwas=mutate(tr1_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-tr1_sh_gwas=mutate(tr1_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-tr1_sh_gwas=mutate(tr1_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
-			 eaf=gwas_reordered[[1]]$eaf)
+tr_sh_gwas=lapply(n_traits, function(x) GWAS_linear_combination(a=position[x,]-alphas*slope[x],beta_a=betas,se=se,covm=as.matrix(gcov),N=sample_size))
 
-head(tr1_sh_gwas,n=2)
-data.table::fwrite(
-	tr1_sh_gwas, 
+tr_sh_gwas=lapply(tr_sh_gwas, function(x) mutate(x, Z=b/se,p=pchisq(Z^2,1,low=F)))
+tr_sh_gwas=lapply(tr_sh_gwas, function(x) mutate(x, SNP=gwas_reordered[[1]]$rs_id))
+tr_sh_gwas=lapply(tr_sh_gwas, function(x) mutate(x, A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp, eaf=gwas_reordered[[1]]$eaf))
+#b1=cov_gi_alpha(alphas,1,as.matrix(covm))
+#tr1_sh_gwas=GWAS_linear_combination(a=c(1,0,0,0)-alphas*b1,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
+#tr1_sh_gwas=mutate(tr1_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+#tr1_sh_gwas=mutate(tr1_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
+#tr1_sh_gwas=mutate(tr1_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp, eaf=gwas_reordered[[1]]$eaf)
+lapply(tr_sh_gwas,function(x) head(x,n=2))
+lapply(n_traits, function(x) data.table::fwrite(
+	tr_sh_gwas[[x]], 
 	row.names=F,
-	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4049-SH_GWAS.txt',
-	sep = '\t')
-	
-b2=cov_gi_alpha(alphas,2,as.matrix(covm))
-
-tr2_sh_gwas=GWAS_linear_combination(a=c(0,1,0,0)-alphas*b2,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
-
-tr2_sh_gwas=mutate(tr2_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-tr2_sh_gwas=mutate(tr2_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-tr2_sh_gwas=mutate(tr2_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
-			 eaf=gwas_reordered[[1]]$eaf)
-
-head(tr2_sh_gwas,n=2)
-data.table::fwrite(
-	tr2_sh_gwas, 
-	row.names=F,
-	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4050-SH_GWAS.txt',
-	sep = '\t')
-
-b3=cov_gi_alpha(alphas,3,as.matrix(covm))
-
-tr3_sh_gwas=GWAS_linear_combination(a=c(0,0,1,0)-alphas*b3,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
-
-tr3_sh_gwas=mutate(tr3_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-tr3_sh_gwas=mutate(tr3_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-tr3_sh_gwas=mutate(tr3_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
-			 eaf=gwas_reordered[[1]]$eaf)
-
-head(tr3_sh_gwas,n=2)
-data.table::fwrite(
-	tr3_sh_gwas, 
-	row.names=F,
-	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4058-SH_GWAS.txt',
-	sep = '\t')
-
-b4=cov_gi_alpha(alphas,4,as.matrix(covm))
-
-tr4_sh_gwas=GWAS_linear_combination(a=c(0,0,0,1)-alphas*b4,beta_a=betas,se=se,covm=as.matrix(covm),N=sample_size)
-
-tr4_sh_gwas=mutate(tr4_sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-tr4_sh_gwas=mutate(tr4_sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-tr4_sh_gwas=mutate(tr4_sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
-			 eaf=gwas_reordered[[1]]$eaf)
-
-head(tr4_sh_gwas,n=2)
-data.table::fwrite(
-	tr4_sh_gwas, 
-	row.names=F,
-	file = '../../data/anthropometry_results/four_traits/linear_combination/Tr4179-SH_GWAS.txt',
-	sep = '\t')
+	file = paste0('../../data/anthropometry_results/four_traits/linear_combination/Tr',colnames(gcov)[x],'-SH_GWAS.txt',
+	sep = '\t')))
