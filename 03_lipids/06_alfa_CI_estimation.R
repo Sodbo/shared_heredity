9387639ec6abb9cff59b0ea46ff455405f5d5240
SofiaFeoktistova <sayfutdinovas@gmail.com>
2020-02-17 12:28:44 +0700

    code for lipids

--- 03_lipids/06_alfa_CI_estimation.R
+++ 03_lipids/06_alfa_CI_estimation.R
@@ -9,64 +9,9 @@ se <-as.matrix(read.table('~/polyomica/projects/shared_heredity/data/02_Lipids/g
 #gcovm <-read.table('~/polyomica/projects/shared_heredity/data/02_Lipids/gene_cov_matrix.txt', check.names=F)
 #phem <-read.table('~/polyomica/projects/shared_heredity/data/02_Lipids/pheno_corr_matrix.txt', check.names=F)
 
-function_for_estimation_of_alfa_CI=function(gcovm,phem,se,lll=0,N_permut=1000){
-  
-  #some fucntions
-  noise_sd=function(se){
-    n=nrow(se)
-    out=array(NA,c(n,n))
-    for (i in 1:(n)){
-      j=i
-      for (j in (i):n){
-        out[i,j]=rnorm(1,mean=0,sd=se[i,j])
-      }
-    }
-    out[lower.tri(out)]=0
-    out_1=t(out)
-    out=out+out_1
-    diag(out)=diag(out)/2
-    out
-  }
-  
-  
-  #core
+source("../00_core_functions/function_for_estimation_of_alfa_CI.R")
 
-  out=array(NA,c(N_permut,nrow(gcovm)))
-
-  i=1
-  for (i in 1:N_permut){
-    
-    NS=noise_sd(se)
-    Z_p=gcovm+NS
-    
-    #shrinkage  
-    lambda=rep((1-lll),nrow(Z_p))
-    lambda=sqrt(lambda)%o%sqrt(lambda)
-    diag(lambda)=1
-    Z_p=Z_p*lambda
-	
-if ((sum(diag(Z_p)<0.001))==0 & det(Z_p)>0){
-	alfas=shared_heredity(CovGenTr=Z_p, CorPhenTr=phem)$alphas[2,]
-    
-    out[i,]=alfas
-}
-}
-
-#if na out print YOU HAVE NA
-
-out=na.omit(out)
- j=1
-  CIs=rep(0,nrow(gcovm))
-  for (j in 1:nrow(gcovm)){
-    qq=quantile(x=out[,j],probs = c(0.025,0.975))
-    CIs[j]=abs(qq[1]-qq[2])/2
-    names(CIs)<-colnames(gcovm)
-  } 
-  
-  return(CIs)
-}
-
-  (res<-function_for_estimation_of_alfa_CI(A0,CorPhenTr,se,N_permut = 200))
+res<-function_for_estimation_of_alfa_CI(A0,CorPhenTr,se,N_permut = 200)
 names(res)=colnames(A0)
 write.table(res,'~/polyomica/projects/shared_heredity/data/02_Lipids/CIs_for_3_traits.txt',quote=F)
 
---
+++ 03_lipids/run_upload.sh
@@ -0,0 +1,2 @@
+run_upload \
+--gwas-path=~/polyomica/projects/shared_heredity/data/02_Lipids/unification/unification_lipids_sh_done.csv
