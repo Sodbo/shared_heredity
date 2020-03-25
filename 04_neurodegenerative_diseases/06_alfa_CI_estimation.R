006d4a0f2a7cf654d2956b1e5049352fb778ae13
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-03-26 01:55:50 +0700

    Modified scripts using new alpha coeffitients and core function for logistic regression

--- 04_neurodegenerative_diseases/06_alfa_CI_estimation.R
+++ 04_neurodegenerative_diseases/06_alfa_CI_estimation.R
@@ -5,7 +5,7 @@
 #N_permut - N of permutations
 
 source("05_shared_heredity.R", chdir = F)
-se <-as.matrix(read.table('~/polyomica/projects/shared_heredity/data/neurodegenerative_diseases/several_traits/gene_cov_se_matrix.txt', check.names=F))
+se <-as.matrix(read.table('~/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/gene_cov_se_matrix.txt', check.names=F))
 #gcovm <-read.table('~/polyomica/projects/shared_heredity/data/02_Lipids/gene_cov_matrix.txt', check.names=F)
 #phem <-read.table('~/polyomica/projects/shared_heredity/data/02_Lipids/pheno_corr_matrix.txt', check.names=F)
 
@@ -13,5 +13,5 @@ source("../00_core_functions/function_for_estimation_of_alfa_CI.R")
 
 res<-function_for_estimation_of_alfa_CI(A0,CorPhenTr,se,N_permut = 100)
 names(res)=colnames(A0)
-write.table(res,'~/polyomica/projects/shared_heredity/data/neurodegenerative_diseases/several_traits/CIs_for_3_traits.txt',quote=F)
+write.table(res,'~/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/CIs_for_3_traits.txt',quote=F)
 
