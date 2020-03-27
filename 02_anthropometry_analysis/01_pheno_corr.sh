dfebc5b380c8725f31f81de2e829315459d9532b
TiysEvgeny <tiys@ngs.ru>
2020-03-27 18:09:21 +0700

    Replace command alias with full path to the script of GWAS-Map

--- 02_anthropometry_analysis/01_pheno_corr.sh
+++ 02_anthropometry_analysis/01_pheno_corr.sh
@@ -2,6 +2,6 @@ output_path=$1
 shift
 echo 'Output directory: '$output_path
 echo 'Gwas IDs to analyse:' $*
-run_pheno_corr \
+python3 ~/code_folder/gwas_v2/gwas_v2/analysis/database_interfaces/run_phen_corr.py \
 	--gwas-ids $(echo $* | tr ' ' ',')\
 	--output-path ${output_path}phen_corr_res.txt > ${output_path}01.log
