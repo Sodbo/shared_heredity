9e3671c99ce50904b7fb973759d7ea7a3003a22b
TiysEvgeny <tiys@ngs.ru>
2020-03-27 18:06:10 +0700

    Change description of the pipeline and change start parameters

--- 02_anthropometry_analysis/00_start.sh
+++ 02_anthropometry_analysis/00_start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# This script starts a pipline script to count matrices of genetic and phenotypic covariations required for shared genetic component and also start the script to calculate it. Check the starting parameters of this script enumerating 05 if necessary.
 # Path to output directory should be given as the first command line argument
 # Ids of GWAS should also be given as the command line arguments after path.
 # Necessary R libraries should be installed before the first run.
-# Results will be written to the following files:
+# 
+# Results will be written to the following files (path to them you should give as the first parameter):
 # gene_corr_matrix.txt
 # gene_corr directory
 # gene_cov_matrix.txt
@@ -12,7 +14,7 @@
 # w.txt
 
 ## Example
-## source 14b_pipeline_for_matrix_calculation.sh ../../data/anthropometry_results/four_traits/Traits_minus_SH_test/ 153 154 155 156
-
-source pipeline_for_calculation_of_matrices.sh ../../data/anthropometry_results/four_traits/ 1 2 3 4
+## source pipeline_for_matrix_calculation.sh ../../data/01_anthropometry_results/Traits_minus_SH_test/ 153 154 155 156
+export PROD=T #Option is necessary for GWASes uploaded to GWAS-Map
+source pipeline_for_calculation_of_matrices.sh ../../data/01_anthropometry_results/ 4049 4050 4058 4179
 
