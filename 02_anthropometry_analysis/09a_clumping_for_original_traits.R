7f010918bcab2f8c0be3935dd1e9feb87c9ab1d5
TiysEvgeny <tiys@ngs.ru>
2020-03-30 12:04:15 +0700

    Remove body of the function from this file and add link to the 00_core_function and change the path to the result folder

--- 02_anthropometry_analysis/09a_clumping_for_original_traits.R
+++ 02_anthropometry_analysis/09a_clumping_for_original_traits.R
@@ -1,58 +1,12 @@
 #Clumping for the original traits
 
 library(data.table) 
-
-path <- "../data/anthropometry_results/four_traits/GWAS/scaled_filtered/"
+source('../00_core_functions/clumping.R')
+path <- "../../data/01_anthropometry_results/GWAS/scaled_filtered/"
 input_file_name<-c('ID_4049.csv','ID_4050.csv','ID_4058.csv','ID_4179.csv')
 result_file_name<-'Clumping_for_all_orignal_traits_and_SH.txt'
 out <- NULL
 thr <- 5e-8
-
-
-function_for_shlop_28_12_2017 <- function(locus_table,p_value="p_ma",pos="bp",snp="rs_id", delta=5e5,chr="chr",thr=5e-8,trait=NULL){
-	locus_table[,p_value] <- as.numeric(locus_table[,p_value])
-	if (!is.null(trait)){
-		traits="traits"
-		locus_table=cbind(locus_table,traits=locus_table[,trait])
-		locus_table[,traits]=as.character(locus_table[,traits])
-	}
-	out=locus_table[0,]
-		locus_table=locus_table[locus_table[,p_value]<=thr,]
-		i=1
-		if (nrow(locus_table)>0){
-			locus_table[,pos]=as.numeric(locus_table[,pos])
-			locus_table[,p_value]=as.numeric(locus_table[,p_value])
-			Zx <-locus_table
-			Zx=Zx[order(Zx[,p_value]),]
-			#n_traits=1 
-			#Zx=cbind(Zx,n_traits)
-			i=1
-			
-			while (nrow(Zx)>0){
-				ind=which((abs(Zx[i,pos]-Zx[,pos])<=delta)&(Zx[i,chr]==Zx[,chr]))
-				
-				if (!is.null(trait)){
-					Zx[i,traits]=paste(unique(Zx[ind,trait]),collapse = ";")
-				}
-				
-				out=rbind(out,Zx[i,])
-				Zx=Zx[-ind,]
-			}
-				
-			rownames(out)=as.character(out[,snp])
-		}
-		if (!is.null(trait)){
-			j=1
-			out=cbind(out,Ntraits=1)
-			out[,"Ntraits"]=as.numeric(out[,"Ntraits"])
-			for (j in 1:nrow(out)){
-				trs=unique(unlist(strsplit(out[j,traits],split = ";")))
-				out[j,traits]=paste(trs,collapse = ";")
-				out[j,"Ntraits"]=length(trs)
-			}
-		}
-		return(out)
-	}
 
 for (input in input_file_name) {
 	sst_file <- paste0(path,input)
