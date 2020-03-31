e62410da19eed11c96f1a8b0ff37c7cd9b70b9e9
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-03-31 22:20:13 +0700

    Keep v2 code as more fast and optimal. Renamed script

--- 04_neurodegenerative_diseases/11_grep_clumped_SNPs.sh
+++ 04_neurodegenerative_diseases/11_grep_clumped_SNPs.sh
@@ -1,9 +1,8 @@
 # This script is to grep clumped SNPs from original traits and shared heredity
 
 path='/mnt/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases'
-while IFS= read -r line; do
-	grep -w "$line" $path'/BIP/03_gc_corrected/bip_gc_corrected.csv' >> $path'/BIP/04_clumped_snps/bip_clumped_snps_only.txt'
-	grep -w "$line" $path'/MDD/03_gc_corrected/mdd_gc_corrected.csv' >> $path'/MDD/04_clumped_snps/mdd_clumped_snps_only.txt'
-	grep -w "$line" $path'/SCZ/03_gc_corrected/scz_gc_corrected.csv' >> $path'/SCZ/04_clumped_snps/scz_clumped_snps_only.txt'
-	grep -w "$line" $path'/several_traits/linear_combination/03_gc_corrected/sh_gc_corrected.csv' >> $path'/several_traits/linear_combination/04_clumped_snps/sh_clumped_snps_only.txt'
-done < $path'/several_traits/Clumped_SNPs_original_traits_and_SH.txt'
+grep -w -f $path'/several_traits/Clumped_SNPs_original_traits_and_SH.txt' $path'/BIP/03_gc_corrected/bip_gc_corrected.csv' >> $path'/BIP/04_clumped_snps/bip_clumped_snps_only_v2.txt'
+grep -w -f $path'/several_traits/Clumped_SNPs_original_traits_and_SH.txt' $path'/MDD/03_gc_corrected/mdd_gc_corrected.csv' >> $path'/MDD/04_clumped_snps/mdd_clumped_snps_only_v2.txt'
+grep -w -f $path'/several_traits/Clumped_SNPs_original_traits_and_SH.txt' $path'/SCZ/03_gc_corrected/scz_gc_corrected.csv' >> $path'/SCZ/04_clumped_snps/scz_clumped_snps_only_v2.txt'
+grep -w -f $path'/several_traits/Clumped_SNPs_original_traits_and_SH.txt' $path'/several_traits/linear_combination/03_gc_corrected/sh_gc_corrected.csv' >> $path'/several_traits/linear_combination/04_clumped_snps/sh_clumped_snps_only_v2.txt'
+
