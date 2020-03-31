52f9c7b1df3ca38d4ad0ccda7821f0a0297b59bc
TiysEvgeny <tiys@ngs.ru>
2020-03-31 16:18:53 +0700

    Add third version of clumping for testing

--- 00_core_functions/README.md
+++ 00_core_functions/README.md
@@ -4,3 +4,6 @@ we would like to understand differences between this and new v2 version.
 
 ### clumping_v2.R
 Function for clumping with simplified code in comparison to old one from commit 10300ae175d92e61a5e66ebd19e8cb9f11832829 . It was tested and show the same results as previous, but after analysis of real data there was a difference in results. We keep two versions of these function in this version of repository to understand whether there are some differences.
+
+### clumping_v3.R
+Function for clumping from commit 10300ae175d92e61a5e66ebd19e8cb9f11832829 . Functions clumping_v1.R clumping_v2.R give identical result, so it was decided to test this variant to exclude mistake during its first using.
---
+++ 00_core_functions/clumping_v3.R
@@ -0,0 +1,45 @@
+function_for_shlop_28_12_2017 <- function(locus_table,p_value="p_ma",pos="bp",snp="rs_id", delta=5e5,chr="chr",thr=5e-8,trait=NULL){
+	locus_table[,p_value] <- as.numeric(locus_table[,p_value])
+	if (!is.null(trait)){
+		traits="traits"
+		locus_table=cbind(locus_table,traits=locus_table[,trait])
+		locus_table[,traits]=as.character(locus_table[,traits])
+	}
+	out=locus_table[0,]
+		locus_table=locus_table[locus_table[,p_value]<=thr,]
+		i=1
+		if (nrow(locus_table)>0){
+			locus_table[,pos]=as.numeric(locus_table[,pos])
+			locus_table[,p_value]=as.numeric(locus_table[,p_value])
+			Zx <-locus_table
+			Zx=Zx[order(Zx[,p_value]),]
+			#n_traits=1 
+			#Zx=cbind(Zx,n_traits)
+			i=1
+			
+			while (nrow(Zx)>0){
+				ind=which((abs(Zx[i,pos]-Zx[,pos])<=delta)&(Zx[i,chr]==Zx[,chr]))
+				
+				if (!is.null(trait)){
+					Zx[i,traits]=paste(unique(Zx[ind,trait]),collapse = ";")
+				}
+				
+				out=rbind(out,Zx[i,])
+				Zx=Zx[-ind,]
+			}
+				
+			rownames(out)=as.character(out[,snp])
+		}
+		if (!is.null(trait)){
+			j=1
+			out=cbind(out,Ntraits=1)
+			out[,"Ntraits"]=as.numeric(out[,"Ntraits"])
+			for (j in 1:nrow(out)){
+				trs=unique(unlist(strsplit(out[j,traits],split = ";")))
+				out[j,traits]=paste(trs,collapse = ";")
+				out[j,"Ntraits"]=length(trs)
+			}
+		}
+		return(out)
+	}
+ 
