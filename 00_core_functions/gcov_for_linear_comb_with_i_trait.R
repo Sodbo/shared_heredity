9a5ed03c8e854933d97f362ce438c8e717d341d6
TiysEvgeny <tiys@ngs.ru>
2020-04-01 17:40:24 +0700

    Correct function name

--- 00_core_functions/gcov_for_linear_comb_with_i_trait.R
+++ 00_core_functions/gcov_for_linear_comb_with_i_trait.R
@@ -1,5 +1,5 @@
 # genetic covariacne of linear combination with a coeficient with i trait
-cov_gi_alfa=function(a,i,covm=covm){
+cov_gi_alpha=function(a,i,covm=covm){
   cov_gi_sum_giai=sum(covm[i,]*a)
   var_gi=covm[i,i]
   var_sum_giai=sum(covm*(a%o%a)) #gen(a)
