675971b37168de85c3bcedd6d28f9865807ee17f
Yakov Tsepilov <drosophila.simulans@gmail.com>
2020-04-06 02:22:12 +0700

    v2 modification

--- 00_core_functions/linear_combination_v2.R
+++ 00_core_functions/linear_combination_v2.R
@@ -10,10 +10,10 @@ GWAS_linear_combination_v2=function(a,beta_a,se,var_y=NULL,covm,N){
 	}
 	
 	covm=(1/(sqrt(diag(covm))%o%sqrt(diag(covm))))*covm
-	
-	N_min_index=sapply(snp_row, function(x) which.min(N[x,]))
+		
+	#N_min_index=sapply(snp_row, function(x) which.max(N[x,]))
 	#se_N_min<-sapply(snp_row, function (x) se[x,N_min_index[x]])
-	N_min<-sapply(snp_row, function(x) N[x,N_min_index[x]])
+	#N_min<-sapply(snp_row, function(x) N[x,N_min_index[x]])
 	#beta_N_min<-sapply(snp_row, function(x) beta_a[x,N_min_index[x]])
 	
 	#var_y_N_min<-sapply(snp_row, function(x) var_y[N_min_index[x]])
@@ -31,8 +31,16 @@ GWAS_linear_combination_v2=function(a,beta_a,se,var_y=NULL,covm,N){
 	b=b/sqrt(var_y_a)
 	sen=sen/sqrt(var_y_a)
 	
-	out=cbind(b=b,se=sen,N=N_min)
-	colnames(out)=c("b","se","N")
+	N_geom_min=apply(N,MAR=1,function(x){prod(x)^(1/length(x))})
+	N_ariph_min=apply(N,MAR=1,function(x){mean(x)})
+	N_min=apply(N,MAR=1,function(x){min(x)})
+	N_max=apply(N,MAR=1,function(x){max(x)})
+	ind=which(abs(b/sen)<2)
+	N_from_se=median(1/(sen[ind]^2))
+	
+	out=cbind(b=b,se=sen,N=N_from_se,N_geom_mean=N_geom_min,N_mean=N_ariph_min,N_min,N_max)
+	colnames(out)=c("b","se","N","N_geom_mean","N_mean","N_min","N_max")
 	out=as.data.frame(out)
 	return(out)
-} 
+}
+
