05b7afa0df70213bb1ff41c9eef1a68801fcbe17
TiysEvgeny <tiys@ngs.ru>
2020-04-14 15:38:49 +0700

    Correct paths to the actual ones

--- 02_anthropometry_analysis/24_joint_clumping_and_enrichment.R
+++ 02_anthropometry_analysis/24_joint_clumping_and_enrichment.R
@@ -1,24 +1,25 @@
 # Aim of this script is to obtain all figures and tables for SH enrichment
 
-library(data.table)
-library(dplyr)
+if (!require('data.table')) install.packages('data.table'); library('data.table')
+if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
+if (!require('pROC')) install.packages('pROC'); library('pROC')
 
-setwd('/mnt/polyomica/projects/shared_heredity/data/01_anthropometry_results/GWAS/scaled_filtered/')
-path_for_output_results="/mnt/polyomica/projects/shared_heredity/data/01_anthropometry_results/GWAS/scaled_filtered/joint_clumping_and_enrichment/"
-
+path_for_output_results="../../data/01_anthropometry_results/GWAS/scaled_filtered/joint_clumping_and_enrichment_new/"
+dir.create(path_for_output_results, showWarnings = FALSE)
+path_to_gwas='../../data/01_anthropometry_results/GWAS/scaled_filtered/'
 #GC corrected original GWASes
 input_file_name<-c('ID_4049_gc_corrected.csv','ID_4050_gc_corrected.csv','ID_4058_gc_corrected.csv','ID_4179_gc_corrected.csv')
-gwas<-lapply(input_file_name, fread)
+gwas<-lapply(paste0(path_to_gwas, input_file_name), fread)
 
 #GWAS for shared heredity
-gwas_sh=fread("/mnt/polyomica/projects/shared_heredity/data/01_anthropometry_results/linear_combination/SH_GWAS.txt",data.table=F)
+gwas_sh=fread("../../data/01_anthropometry_results/linear_combination/SH_GWAS.txt",data.table=F)
 
 #eaf filtering for 
 ind=which(gwas_sh$eaf>=0.01 & (1-gwas_sh$eaf)<=0.99)
 gwas_sh=gwas_sh[ind,]
 
-gwas<-lapply(1:length(gwas), function(x){
-					tmp=gwas[[x]];
+gwas<-lapply(gwas, function(x){
+					tmp=x;
 					ind=which(tmp$eaf>=0.01 & (1-tmp$eaf)<=0.99);
 					tmp[ind,];
 					} )
@@ -47,7 +48,7 @@ pos=gwas_sh$pos
 #	orig_traits_names=input_file_name;path_output=path_for_output_results
 
 
-source("/mnt/polyomica/projects/shared_heredity/ytsepilov_src/00_core_functions/joint_function_for_enrichment_and_auc.R")
+source("../00_core_functions/joint_function_for_enrichment_and_auc.R")
 	
 out=joint_function(pval_orig=pval_orig,pval_sh=pval_sh,chr=chr,pos=pos,thr_sh_hits=1e-5,thr_sh_sig=5e-8,
 	orig_traits_names=input_file_name,path_output=path_for_output_results,delta=5e5,SNPs=snps)
