b2c87123cd4f6fa1db5fbee2623328a39127994c
TiysEvgeny <tiys@ngs.ru>
2020-04-30 18:55:37 +0700

    Correct paths

--- 02_anthropometry_analysis/11_upload_sh_to_db.sh
+++ 02_anthropometry_analysis/11_upload_sh_to_db.sh
@@ -1,14 +1,14 @@
 #this script upload SH gwas into test db in order to apply gc_correction
+path='../../data/01_anthropometry_results/five_traits/linear_combination/'
+descr_folder=$path'01_descriptors'
+uni_folder=$path'02_unification_out'
 
-descr_folder='../../data/01_anthropometry_results/linear_combination/01_descriptors'
-uni_folder='../../data/01_anthropometry_results/linear_combination/02_unification_out'
 
-
-mkdir ${uni_folder}/SH
+mkdir -p ${uni_folder}/SH
 
 run_uni_qc_rep \
---gwas-path=../../data/01_anthropometry_results/linear_combination/SH_GWAS.txt \
---mapping-path=../../data/01_anthropometry_results/linear_combination/mapping.json \
+--gwas-path=${path}SH_GWAS.txt \
+--mapping-path=${path}mapping.json \
 --descriptors-path=${descr_folder}/descriptor_SH.json \
 --output-dir=${uni_folder}/SH/ \
 --output-file=SH \
