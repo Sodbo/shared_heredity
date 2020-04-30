4d020813565615797eb75448716a8391a6b385be
TiysEvgeny <tiys@ngs.ru>
2020-04-30 16:30:00 +0700

    Replace path to actual ones and replace magical constant with right code

--- 02_anthropometry_analysis/08a_validity_check.R
+++ 02_anthropometry_analysis/08a_validity_check.R
@@ -1,24 +1,24 @@
 #checking if old results is differnt from new ones.
 #
-
+path<-'../../data/01_anthropometry_results/five_traits/'
 library(data.table)
 
-sh<-fread('../../data/01_anthropometry_results/linear_combination/SH_GWAS.txt')
-sh_old<-fread('../../data/01_anthropometry_results/linear_combination/old_results/SH_GWAS.txt')
-png('../../data/01_anthropometry_results/linear_combination/old_results/new_results_validity/beta.png')
+sh<-fread(paste0(path,'linear_combination/SH_GWAS.txt'))
+sh_old<-fread('../../data/01_anthropometry_results/linear_combination/SH_GWAS.txt')
+png(paste0(path,'linear_combination/validity_check/beta.png'))
 plot(sh$b, sh_old$b, xlab='new', ylab='old', main='beta')
 dev.off()
-png('../../data/01_anthropometry_results/linear_combination/old_results/new_results_validity/se.png')
+png(paste0(path,'linear_combination/validity_check/se.png'))
 plot(sh$se, sh_old$se, xlab='new', ylab='old', main='se')
 dev.off()
-png('../../data/01_anthropometry_results/linear_combination/old_results/new_results_validity/Z.png')
+png(paste0(path,'linear_combination/validity_check/Z.png'))
 plot(sh$Z,sh_old$Z)
 dev.off()
 
 source('../00_core_functions/heritability_of_linear_combination.R')
-covm<-read.delim('../../data/01_anthropometry_results/gene_cov_matrix.txt',sep=' ', row.names=1)
-phem<-read.delim('../../data/01_anthropometry_results/pheno_corr_matrix.txt',sep=' ', row.names=1)
-a<-read.delim('../../data/01_anthropometry_results/alphas.txt',sep=' ', row.names=1)
-a<-as.numeric(a[2,1:4])
+covm<-read.delim(paste0(path,'gene_cov_matrix.txt'),sep=' ', row.names=1)
+phem<-read.delim(paste0(path,'pheno_corr_matrix.txt'),sep=' ', row.names=1)
+a<-read.delim(paste0(path,'alphas.txt'),sep='', row.names=1)
+a<-as.numeric(a[2,])
 
 H2(a,covm=covm,phem=phem)
