685876cfb1fb34dda2c4da52e5454feed7a29597
TiysEvgeny <tiys@ngs.ru>
2020-05-07 17:54:40 +0700

    Correct paths and add defining  of gwas_id order in .csv and in the directory

--- 02_anthropometry_analysis/10_p_val_gc_correction.R
+++ 02_anthropometry_analysis/10_p_val_gc_correction.R
@@ -1,11 +1,22 @@
-library(data.table)
-path_to_result_directory<-'../../data/01_anthropometry_results/GWAS/scaled_filtered/'
-gwas_files<-list.files(path_to_result_directory, full.names=T, pattern="ID_\\d+.csv")
-intercept_data<-read.csv('../../data/01_anthropometry_results/intrecept_data.csv') 
-gwas<-lapply(gwas_files, fread)
-chi=lapply(gwas, function(x) qchisq(x$p, df=1, lower.tail=F))
+#This script makes new summary statistics file with column p_gc containing p-values corrected for genomic control.
+# To run the script be sure the order of files in directory correspondes the order of gwas_ids in .csv files
 
-chi_gc=lapply(1:length(chi), function(x) chi[[x]]/intercept_data$intercept[x])
+sh_gwas_id=201
+
+library(data.table)
+path_to_result_directory='../../data/01_anthropometry_results/five_traits/GWAS/'
+gwas_files<-list.files(path_to_result_directory, full.names=T, pattern="ID_\\d+.csv")
+intercept_data<-read.csv(paste0(path_to_result_directory,'intrecept_data.csv')) 
+gwas<-lapply(gwas_files, fread)
+
+sh<-fread('../../data/01_anthropometry_results/five_traits/linear_combination/SH_GWAS.txt')
+gwas<-c(gwas,list(sh))
+
+names(gwas)<-regmatches(gwas_files, regexpr('(?<=ID_)(\\d+)(?=\\.csv)', gwas_files, perl=T))
+names(gwas)[length(gwas)]=sh_gwas_id
+
+chi=lapply(gwas, function(x) qchisq(x$p, df=1, lower.tail=F))
+chi_gc=lapply(1:length(chi), function(x) chi[[x]]/intercept_data$intercept[intercept_data$gwas_id==names(gwas)[x]])
 gwas=lapply(1:length(gwas), function(x) {
 	gwas[[x]]$p_gc=pchisq(chi_gc[[x]], df=1, lower.tail=F)
 	return (gwas[[x]])
@@ -14,7 +25,7 @@ gwas=lapply(1:length(gwas), function(x) {
 lapply(1:length(gwas), function(x) data.table::fwrite(
 		gwas[[x]], 
 		row.names=F,
-		file = paste0('../../data/01_anthropometry_results/GWAS/scaled_filtered/ID_',intercept_data$gwas_id[x],'_gc_corrected.csv'),
+		file = paste0(path_to_result_directory,'ID_',intercept_data$gwas_id[x],'_gc_corrected.csv'),
 		sep = '\t')
 	)
 
