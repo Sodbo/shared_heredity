62946446e3f7aa985bf055078ffa88e227744dcb
TiysEvgeny <tiys@ngs.ru>
2020-05-07 18:11:04 +0700

    Replace core function script with script working with pesonal db

--- 02_anthropometry_analysis/12_check_sh_intercept_for_gc_correction.sh
+++ 02_anthropometry_analysis/12_check_sh_intercept_for_gc_correction.sh
@@ -1,1 +1,9 @@
-source ../00_core_functions/p_correction_for_gc.sh ../../data/01_anthropometry_results/linear_combination/ 186
+#Script to calculate intercept to answer whether correction is necessary.
+
+gwas_ids=201,191,192,193,194,199 # comma separated
+output_path='../../data/01_anthropometry_results/five_traits/linear_combination/'
+shift
+echo 'Output directory: '$output_path
+echo 'Gwas IDs to analyse:' $gwas_ids
+run_ldscore --h2 --gwas-id=$gwas_ids --overwrite
+echo 'select * from gwas.ldsc where gwas_id in ('$gwas_ids');' | psql -U gwas_user -d test_tiysgwas -h 172.25.8.65 -A -F , --pset footer -o ${output_path}intrecept_data.csv
