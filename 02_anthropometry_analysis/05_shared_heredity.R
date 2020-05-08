9968c86fd8a32297e5f4a81f2653e8b58c8c1437
TiysEvgeny <tiys@ngs.ru>
2020-05-08 12:43:21 +0700

    Add output GIP1 to file

--- 02_anthropometry_analysis/05_shared_heredity.R
+++ 02_anthropometry_analysis/05_shared_heredity.R
@@ -4,10 +4,12 @@ CorPhenTr <- as.matrix(read.table(paste0(path,'pheno_corr_matrix.txt'), check.na
 A0 <- as.matrix(read.table(paste0(path,'gene_cov_matrix.txt'), check.names=F))
 output_alpha<-paste0(path,'alphas.txt')
 output_w<-paste0(path,'weights.txt')
+output_gip<-paste0(path, 'GIP1.txt')
 
 
  x<-shared_heredity(CovGenTr=A0, CorPhenTr=CorPhenTr)
 print(x)  
 write.table(x$alphas,output_alpha,quote=F)
 write.table(x$weights,output_w,quote=F)
+write.table(x$GIPs$GIP_coeff[,'GIP1'], output_gip, quote=F)
 x$alphas
