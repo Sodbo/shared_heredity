e522eb22c24410747eaeb8be4cfc1bd9f8e4e9e0
TiysEvgeny <tiys@ngs.ru>
2020-05-11 16:15:29 +0700

    Replace alpha vector for shared heredity with GIP1 vector for general components

--- 02_anthropometry_analysis/07a_linear_combination_gip1.r
+++ 02_anthropometry_analysis/07a_linear_combination_gip1.r
@@ -4,11 +4,11 @@ library('data.table')
 library(dplyr)
 source("../00_core_functions/linear_combination_v3.R")
 
-path_to_result_directory<-'../../data/01_anthropometry_results/five_traits/GWAS/'
-gwas_files<-list.files(path_to_result_directory, full.names=T, pattern="ID_\\d+.csv")
+path_to_gwas_directory<-'../../data/01_anthropometry_results/five_traits/GWAS/'
+gwas_files<-list.files(path_to_gwas_directory, full.names=T, pattern="ID_\\d+.csv")
 gwas<-lapply(gwas_files, fread)
 
-aa<-read.table('../../data/01_anthropometry_results/five_traits/alphas.txt', row.names=1)
+gip1<-read.table('../../data/01_anthropometry_results/five_traits/GIP1.txt', row.names=1)
 covm<-read.table('../../data/01_anthropometry_results/five_traits/pheno_corr_matrix.txt', row.names=1,  check.names=F)
 
 rs_id<-lapply(gwas, function(x) x$rs_id)
@@ -24,17 +24,17 @@ sample_size <- sapply(gwas_reordered, function(x) x$n)
 
 #eaf is better to use for gwas with max sample size, in this case sample sizes 
 
-sh_gwas=GWAS_linear_combination_Z_based(a=as.numeric(aa[2,]),Z = z,covm=as.matrix(covm),N=sample_size, eaf=gwas_reordered[[1]]$eaf)
+gip1_gwas<-GWAS_linear_combination_Z_based(a=as.numeric(gip1[,1]),Z = z,covm=as.matrix(covm),N=sample_size, eaf=gwas_reordered[[1]]$eaf)
 
-sh_gwas=mutate(sh_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
-sh_gwas=mutate(sh_gwas,SNP=gwas_reordered[[1]]$rs_id)
-sh_gwas=mutate(sh_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
+gip1_gwas=mutate(gip1_gwas,Z=b/se,p=pchisq(Z^2,1,low=F))
+gip1_gwas=mutate(gip1_gwas,SNP=gwas_reordered[[1]]$rs_id)
+gip1_gwas=mutate(gip1_gwas,A1=gwas_reordered[[1]]$ea,A2=gwas_reordered[[1]]$ra,chr=gwas_reordered[[1]]$chr,pos=gwas_reordered[[1]]$bp,
 			 eaf=gwas_reordered[[1]]$eaf)
 
-head(sh_gwas,n=2)
-dir.create('../../data/01_anthropometry_results/five_traits/linear_combination/')
+head(gip1_gwas,n=2)
+dir.create('../../data/01_anthropometry_results/five_traits/GIP1/')
 data.table::fwrite(
-	sh_gwas, 
+	gip1_gwas, 
 	row.names=F,
-	file = '../../data/01_anthropometry_results/five_traits/linear_combination/SH_GWAS.txt',
+	file = '../../data/01_anthropometry_results/five_traits/GIP1/GIP1_GWAS.txt',
 	sep = '\t')
