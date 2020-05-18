581e91a7476fa733a18cf7e0a13653518ecf969e
TiysEvgeny <tiys@ngs.ru>
2020-05-18 19:11:07 +0700

    Correct path and filenames

--- 03_lipids/06_alpha_CI_estimation.R
+++ 03_lipids/06_alpha_CI_estimation.R
@@ -6,7 +6,7 @@
 #lll - shrinkage factor [0;1]
 #N_permut - N of permutations
 
-path <- '/mnt/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/four_traits/'
+path <- commandArgs(trailingOnly = T)[1] # get command arguments set in 00a_start.sh or 16a_start.sh
 
 CorPhenTr <- as.matrix(read.table(paste0(path, 'pheno_corr_matrix.txt'), check.names = F)) # load matrix of phenotypic correlations
 A0 <- as.matrix(read.table(paste0(path, 'gene_cov_matrix.txt'), check.names = F)) # load matrix of genetic covariance
@@ -19,15 +19,17 @@ source("../00_core_functions/shared_heredity.R")
 # for alpha coeffitients of SH
 res <- function_for_estimation_of_alfa_CI(gcovm = A0, phem = CorPhenTr, se = se, N_permut = 100)
 names(res) <- colnames(A0)
-write.table(res, '/mnt/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/four_traits/CIs_for_4_traits.txt', quote = F)
+write.table(res, paste0(path,'CIs_for_',length(A0[1,]),'_traits.txt'), quote = F)
 
 # for alpha coeffitients of GIP1
 res2 <- function_for_estimation_of_alfa_CI_GIP(gcovm = A0, phem = CorPhenTr, se = se, N_permut = 100)
 names(res2) <- colnames(A0)
-write.table(res2, '/mnt/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/four_traits/GIP/CIs_for_4_traits.txt', quote = F)
+ifelse(!dir.exists(file.path(path, 'GIP')), dir.create(file.path(path, 'GIP')), FALSE)
+write.table(res2, paste0(path, 'GIP/CIs_for_',length(A0[1,]),'_traits.txt'), quote = F)
 
 # for alpha coeffitients of MaxH
 res3 <- function_for_estimation_of_alfa_CI_MaxH(gcovm = A0, phem = CorPhenTr, se = se, N_permut = 100)
 names(res3) <- colnames(A0)
-write.table(res3, '/mnt/polyomica/projects/shared_heredity/data/03_neurodegenerative_diseases/several_traits/four_traits/MaxH/CIs_for_4_traits.txt', quote = F)
+ifelse(!dir.exists(file.path(path, 'MaxH')), dir.create(file.path(path, 'MaxH')), FALSE)
+write.table(res3, paste0(path,'MaxH/CIs_for_',length(A0[1,]),'_traits.txt'), quote = F)
 
