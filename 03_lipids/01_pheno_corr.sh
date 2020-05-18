5254f20a5a77aeeea538a5fdddfe51519af88a02
TiysEvgeny <tiys@ngs.ru>
2020-05-18 19:06:35 +0700

    Remove separate log for script 01

--- 03_lipids/01_pheno_corr.sh
+++ 03_lipids/01_pheno_corr.sh
@@ -6,6 +6,7 @@ output_path=$1
 shift
 echo 'Output directory: '$output_path
 echo 'Gwas IDs to analyse:' $*
+
 run_pheno_corr \
 	--gwas-ids $(echo $* | tr ' ' ',')\
-	--output-path ${output_path}phen_corr_res.txt > ${output_path}01.log
+	--output-path ${output_path}phen_corr_res.txt
