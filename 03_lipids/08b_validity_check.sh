22bdcebd7952492f9054d8d9613abd58e97ea655
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-05-19 01:58:43 +0700

    Modified script for minus-traits validity check

--- 03_lipids/08b_validity_check.sh
+++ 03_lipids/08b_validity_check.sh
@@ -3,23 +3,23 @@
 # using LD Score implementd in GWAS-Map (from test container)
 
 # Estimate heritability for LDL-SH
-run_ldscore --h2 --gwas-id=
+run_ldscore --h2 --gwas-id=10
 # 
 
 # Estimate heritability for Triglycerides-SH
-run_ldscore --h2 --gwas-id=
+run_ldscore --h2 --gwas-id=11
 # 
 
 # Estimate heritability for Cholesterol-SH
-run_ldscore --h2 --gwas-id=
+run_ldscore --h2 --gwas-id=12
 # 
 
 # Estimate pairwise genetic correlations for SH and Lipids traits minus SH
-run_ldscore --rg --gwas-id=,9 # sh and LDL-sh
+run_ldscore --rg --gwas-id=10,9 # sh and LDL-sh
 ##  (se )
-run_ldscore --rg --gwas-id=,9 # sh and triglycerides-sh
+run_ldscore --rg --gwas-id=11,9 # sh and triglycerides-sh
 ##  (se )
-run_ldscore --rg --gwas-id=,9 # sh and cholesterol-sh
+run_ldscore --rg --gwas-id=12,9 # sh and cholesterol-sh
 ##  (se )
 
 # compare with the results of 08a script
