e294d0262c5ea15b378a59da43b409ce044cde4c
TiysEvgeny <tiys@ngs.ru>
2020-05-19 15:23:44 +0700

    Add data

--- 03_lipids/07_0b_validity_check_sh.sh
+++ 03_lipids/07_0b_validity_check_sh.sh
@@ -4,14 +4,14 @@
 
 # Estimate heritability for SH
 run_ldscore --h2 --gwas-id=9
-## 
+## h2=0.2126  se=0.0295
 
 # Estimate pairwise genetic correlations for SH and Lipids traits
 run_ldscore --rg --gwas-id=6,9 # sh and LDL
-## 
+## 0.9605
 run_ldscore --rg --gwas-id=7,9 # sh and triglycerides
-## 
+## 0.7404
 run_ldscore --rg --gwas-id=8,9 # sh and choleserol
-## 
+## 1.0177
 
 # compare with the results of 07a script
