7ac9c1373f6bc68a3bf53ab89891b5ccb9c876f5
TiysEvgeny <tiys@ngs.ru>
2020-05-19 10:47:42 +0700

    Copy script from PGC to modify

---
+++ 03_lipids/11a_start.sh
@@ -0,0 +1,21 @@
+# Aim of this script is to run automatic pipeline calculating correlation matrices for 
+# original traits, shared heredity and traits minus shared heredity
+
+#!/bin/bash
+# Path to output directory should be given as the first command line argument
+# Ids of GWAS should also be given as the command line arguments after path.
+# Necessary R libraries should be installed before the first run.
+# Results will be written to the following files:
+# gene_corr_matrix.txt
+# gene_corr directory
+# gene_cov_matrix.txt
+# phen_corr_res.txt
+# pheno_corr_matrix.txt
+# alphas.txt
+# w.txt
+
+## Example
+## source 14b_pipeline_for_matrix_calculation.sh ../../data/anthropometry_results/four_traits/Traits_minus_SH_test/ 153 154 155 156
+
+source 16b_pipeline_for_matrix_calculation.sh ../../../data/03_neurodegenerative_diseases/traits_minus_SH/four_traits/ 551 554 555 617 618 619 620 621 622
+
