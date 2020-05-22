8d4b1118185467d896e180825f4ca69cde437cf5
TiysEvgeny <tiys@ngs.ru>
2020-05-22 16:09:57 +0700

    Add values of h2 and rg

--- 03_lipids/08b_validity_check.sh
+++ 03_lipids/08b_validity_check.sh
@@ -4,22 +4,22 @@
 
 # Estimate heritability for LDL-SH
 run_ldscore --h2 --gwas-id=10
-# 
+# h2=0.0994  (se=0.0198)
 
 # Estimate heritability for Triglycerides-SH
 run_ldscore --h2 --gwas-id=11
-# 
+# h2=0.1714  (se=0.0257)
 
 # Estimate heritability for Cholesterol-SH
 run_ldscore --h2 --gwas-id=12
-# 
+# h2=0.1079  (se=0.0185)
 
 # Estimate pairwise genetic correlations for SH and Lipids traits minus SH
 run_ldscore --rg --gwas-id=10,9 # sh and LDL-sh
-##  (se )
+## -0.4904  (se=0.68)
 run_ldscore --rg --gwas-id=11,9 # sh and triglycerides-sh
-##  (se )
+## 0.1671  (se=0.28)
 run_ldscore --rg --gwas-id=12,9 # sh and cholesterol-sh
-##  (se )
+## 0.0805  (se=0.106)
 
 # compare with the results of 08a script
