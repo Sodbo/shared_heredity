8ef9e413bedf865bcb9264b48b609dd6bebd0d13
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-05-22 13:32:52 +0700

    Validated minus-traits in GWAS-Map

--- 03_lipids/08b_validity_check.sh
+++ 03_lipids/08b_validity_check.sh
@@ -4,22 +4,22 @@
 
 # Estimate heritability for LDL-SH
 run_ldscore --h2 --gwas-id=10
-# 
+# 0.0994 
 
 # Estimate heritability for Triglycerides-SH
 run_ldscore --h2 --gwas-id=11
-# 
+# 0.1714
 
 # Estimate heritability for Cholesterol-SH
 run_ldscore --h2 --gwas-id=12
-# 
+# 0.1079
 
 # Estimate pairwise genetic correlations for SH and Lipids traits minus SH
 run_ldscore --rg --gwas-id=10,9 # sh and LDL-sh
-##  (se )
+## -0.4904 (se 0.693)
 run_ldscore --rg --gwas-id=11,9 # sh and triglycerides-sh
-##  (se )
+## 0.1671 (se 0.27)
 run_ldscore --rg --gwas-id=12,9 # sh and cholesterol-sh
-##  (se )
+## 0.0805 (se 0.106)
 
 # compare with the results of 08a script
