124a479a1f7b27312d9cbb74a20a291ba680da24
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2020-06-18 11:15:31 +0000

    Added scripts for GC

---
+++ 03_lipids/12__start_gc_correction.sh
@@ -0,0 +1,5 @@
+# Aim of this script is to estimate parameters of Genomic Control for original traits and shared heredity
+
+# This code uses core function written for GWAS-Map database
+# there are two variables set here: output directory and GWAS IDs
+source p_correction_for_gc.sh /mnt/polyomica/projects/shared_heredity/data/02_Lipids/three_traits/ 6 7 8 9
