3ba6968f72229f111de01e5b39aa88de525a77f7
Elizaveta Elgaeva <elgaeva.liza@yandex.ru>
2021-06-29 16:43:44 +0700

    Added scripts for DEPICT results visualization

---
+++ 02_anthropometry_analysis/DEPICT/01_antro_heatmap.R
@@ -0,0 +1,149 @@
+setwd("../antro")
+
+
+library("readxl")
+library(data.table)
+library(corrplot)
+
+#FUNCTIONS
+overlap_matrix=function(X){
+  N=length(X)
+  out=array(NA,c(N,N))
+  i=1
+  for (i in 1:N){
+    j=1
+    for (j in 1:N){
+      x=X[[i]]
+      y=X[[j]]
+      out[i,j]=length(intersect(x,y))
+    }
+  }
+  colnames(out)=rownames(out)=names(X)
+  out
+}
+
+scale_matrix=function(x){
+  N=dim(x)[1]
+  out=x
+  i=1
+  for (i in 1:(N)){
+    j=1
+    for (j in (1):N){
+      #print(j)
+      out[i,j]=x[i,j]/(min(x[i,i],x[j,j]))
+    }
+  }
+  out
+}
+#########
+
+x=list.files(pattern = "txt")
+x
+
+thr=c("<0.01","<0.05")
+Y=list(NULL)
+library(data.table)
+
+#[1] "bmi.txt"    "fat.txt"    "hip.txt"    "sh.txt"     "waist.txt" 
+#[6] "weight.txt"
+
+"bmi.txt"
+y=fread("bmi.txt",data.table=F,sep="\t")  
+bmi=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"fat.txt"
+y=fread("fat.txt",data.table=F,sep="\t")  
+fat=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"hip.txt"
+y=fread("hip.txt",data.table=F,sep="\t")  
+hip=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"waist.txt"
+y=fread("waist.txt",data.table=F,sep="\t")  
+waist=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"weight.txt"
+y=fread("weight.txt",data.table=F,sep="\t")  
+weight=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"sh.txt"
+y=fread("sh.txt",data.table=F,sep="\t")  
+sgct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+
+"bmi_ugct.txt"
+y=fread("bmi_ugct.txt",data.table=F,sep="\t")  
+bmi_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"fat_ugct.txt"
+y=fread("fat_ugct.txt",data.table=F,sep="\t")  
+fat_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"hip_ugct.txt"
+y=fread("hip_ugct.txt",data.table=F,sep="\t")  
+hip_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"waist_ugct.txt"
+y=fread("waist_ugct.txt",data.table=F,sep="\t")  
+waist_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+"weight_ugct.txt"
+y=fread("weight_ugct.txt",data.table=F,sep="\t")  
+weight_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+
+#Y=list(bmi,hip,waist,fat,weight,sgct)
+
+Y2=list(bmi,weight,hip,waist,fat,sgct,bmi_ugct,weight_ugct,hip_ugct,waist_ugct,fat_ugct)
+names(Y2)=c("BMI","Weight","Hip","Waist","Fat","SGCT","BMI UGCT","Weight UGCT","Hip UGCT","Waist UGCT","Fat UGCT")
+
+#x=overlap_matrix(Y)
+
+x_b=overlap_matrix(Y2)
+
+#x1=(1/sqrt(diag(x)))%o%(1/sqrt(diag(x)))*x
+#x2=scale_matrix(x)
+
+#x1_b=(1/sqrt(diag(x_b)))%o%(1/sqrt(diag(x_b)))*x_b
+x2_b=scale_matrix(x_b)
+
+#corrplot(x1)
+#corrplot(x2)
+#corrplot(x2_b)
+
+pdf("antro_GS_renamed.pdf",width = 7,height = 7)
+corrplot(x2_b,method = "square",p.mat = x_b,sig.level = -1,insig = "p-value",tl.col="black", cl.cex = 1)
+dev.off()
+
+
+######## OLD
+
+library(VennDiagram)
+venn.diagram(
+  x =list(bmi,hip,waist,fat,weight,sgct),
+  filename = '14_venn_diagramm.png',
+  category.names = c("bmi","hip","waist","fat","weight","sgct")
+)
+
+venn.diagram(
+  x =list(bmi,hip,waist),
+  filename = '14_venn_diagramm.png',
+  category.names = c("bmi","hip","waist")
+)
+
+set1 <- paste(rep("word_" , 100) , sample(c(1:1000) , 100 , replace=F) , sep="")
+set2 <- paste(rep("word_" , 200) , sample(c(1:1000) , 200 , replace=F) , sep="")
+set3 <- paste(rep("word_" , 200) , sample(c(1:1000) , 200 , replace=F) , sep="")
+venn.diagram(
+  x = list(set1, set2, set3),
+  category.names = c("Set 1" , "Set 2 " , "Set 3"),
+  filename = '#14_venn_diagramm.png',
+  output=TRUE
+)
+
+
+
+
+plot(lm(I(1:10)~I(11:2)))
+
---
+++ 03_lipids/DEPICT/01_lipids_heatmap.R
@@ -0,0 +1,107 @@
+setwd("C:/Users/ND/Dropbox/Shared_heredity_manuscript/Figures/figure_GS_heatmaps/lipids/")
+
+library("readxl")
+library(data.table)
+library(corrplot)
+
+#FUNCTIONS
+overlap_matrix=function(X){
+  N=length(X)
+  out=array(NA,c(N,N))
+  i=1
+  for (i in 1:N){
+    j=1
+    for (j in 1:N){
+      x=X[[i]]
+      y=X[[j]]
+      out[i,j]=length(intersect(x,y))
+    }
+  }
+  colnames(out)=rownames(out)=names(X)
+  out
+}
+
+scale_matrix=function(x){
+  N=dim(x)[1]
+  out=x
+  i=1
+  for (i in 1:(N)){
+    j=1
+    for (j in (1):N){
+      #print(j)
+      out[i,j]=x[i,j]/(min(x[i,i],x[j,j]))
+    }
+  }
+  out
+}
+#########
+
+x=list.files(pattern = "xlsx")
+x
+#[1] "cholesterol-SH.xlsx"   "cholesterol.xlsx"      "LDL-SH.xlsx"           "LDL.xlsx"             
+#[5] "SH.xlsx"               "Triglycerides-SH.xlsx" "Triglycerides.xlsx" 
+
+thr=c("<0.01","<0.05")
+Y=list(NULL)
+
+#orig
+
+y=read_excel("cholesterol.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+cholesterol=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+y=read_excel("Triglycerides.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+triglycerides=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+y=read_excel("LDL.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+LDL=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+#UGCT
+y=read_excel("LDL-SH.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+LDL_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+y=read_excel("cholesterol-SH.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+cholesterol_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+y=read_excel("Triglycerides-SH.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+triglycerides_ugct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+#SGCT
+
+y=read_excel("SH.xlsx",sheet = "genesetenrichment")
+y=as.data.frame(y)
+dim(y)
+sgct=y[y[,"False discovery rate"]%in%thr,"Original gene set ID"]
+
+#HEATMAP
+
+Y2=list(LDL,triglycerides,cholesterol,sgct,LDL_ugct,triglycerides_ugct,cholesterol_ugct)
+names(Y2)=c("LDL","Triglycerides","Cholesterol","SGCT","LDL UGCT","Triglycerides UGCT","Cholesterol UGCT")
+
+x_b=overlap_matrix(Y2)
+x2_b=scale_matrix(x_b)
+library(corrplot)
+
+pdf("lipids_GS_renamed.pdf",width = 7,height = 7)
+corrplot(x2_b,method = "square",p.mat = x_b,sig.level = -1,insig = "p-value",tl.col="black")
+dev.off()
+
+
+
+
+
+
+
+
+
